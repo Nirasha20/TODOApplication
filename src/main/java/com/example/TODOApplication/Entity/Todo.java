875b7e0fbb73f32f8627aa12fea414eacd318c89
Nirasha <demelwgnd.22@uom.lk>
2025-12-10 21:20:41 +0530

    feat: add html form

--- src/main/java/com/example/TODOApplication/Entity/Todo.java
+++ src/main/java/com/example/TODOApplication/Entity/Todo.java
@@ -1,11 +1,9 @@
 package com.example.TODOApplication.Entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
+@Table(name="todotable")
 public class Todo {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)

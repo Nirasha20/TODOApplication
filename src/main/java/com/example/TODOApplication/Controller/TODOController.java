54eee04cbf64a5f7787d0b85ed201a6a8d599a58
Nirasha <demelwgnd.22@uom.lk>
2025-12-12 11:40:27 +0530

    feat: create post method

--- src/main/java/com/example/TODOApplication/Controller/TODOController.java
+++ src/main/java/com/example/TODOApplication/Controller/TODOController.java
@@ -1,7 +1,33 @@
 package com.example.TODOApplication.Controller;
 
+import com.example.TODOApplication.Service.TODOService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class TODOController {
+
+    @Autowired
+    TODOService todoService;
+
+    @RequestMapping(value="/addtodo", method = RequestMethod.POST)
+    @ResponseBody
+    public String createTodo(@ModelAttribute Todo todo) {
+        todoService.saveTodo(todo);
+        return "success";
+
+    }
+//    public String updateTodo() {
+//
+//    }
+//    public String deleteTodo() {
+//
+//    }
+//    public String getAllTodo() {
+//
+//    }
 }
--- src/main/java/com/example/TODOApplication/Repository/TODORepository.java
+++ src/main/java/com/example/TODOApplication/Repository/TODORepository.java
@@ -1,7 +1,18 @@
 package com.example.TODOApplication.Repository;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class TODORepository {
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Transactional
+    public void save(Todo todo) {
+        entityManager.persist(todo);
+    }
+
 }
